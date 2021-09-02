0bd253358442b537330490d28d3a513aa5734d68
kaz-0704` <okk8578@gmail.com>
2021-09-02 21:58:57 +0900

    ログイン機能付けた

--- app.py
+++ app.py
@@ -24,14 +24,19 @@ class User(Base):
 
 class Club(Base):
     __tablename__ = 'clubs'
-    name = Column('name', String(30), primary_key=True)
+    id = Column('id', String(30), primary_key=True)
+    password = Column('pass', String(30))
+    name = Column('name', String(30))
     belonging = Column('belonging', String(30))
+    area = Column('area', String(30))
+    category = Column('category', String(30))
 
 
 class Post(Base):
     __tablename__ = 'posts'
     name = Column('name', String(30), primary_key=True)
     content = Column('cnotent', String(500))
+    
 
  
 
@@ -41,19 +46,43 @@ session = scoped_session(SessionMaker)  # 経路を実際に作成しました
 
 
 
+check = False
+access_club = None
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def main():
-    posts = session.query(Post).all()
-    return render_template("main.html", cont=posts)
+    global check
+    if request.method == "GET":
+        posts = session.query(Post).all()
+        return render_template("main.html", cont=posts, check=check)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    global check
+    if request.method == "GET":
+        return render_template("login.html", check=check)
+    
+    else:
+        id = request.form['id']
+        password = request.form['pass']
+        club = session.query(Club).filter(Club.id=='{}'.format(id))
+        for c in club:  club_pass = c.password
+        if club_pass == password:
+            check = True
+            global access_club
+            access_club = c
+            return render_template("login.html", check=check)
+        else:
+            return render_template("login.html", check=check)
 
 
 
-@app.route("/register", methods=["GET", "POST"])
-def register():
+@app.route("/user_register", methods=["GET", "POST"])
+def user_register():
     if request.method == "GET":
         users = session.query(User).all()
-        return render_template("register.html", cont=users)
+        return render_template("user_register.html", cont=users)
     
     else:
         name = request.form['name']
@@ -61,7 +90,33 @@ def register():
         user = User(name='{}'.format(name), password='{}'.format(password))
         session.add(user)
         session.commit()
-        return redirect("/register")
+        return redirect("/user_register")
+
+
+
+@app.route("/club_register", methods=["GET", "POST"])
+def club_register():
+    if request.method == "GET":
+        clubs = session.query(Club).all()
+        return render_template("club_register.html", cont=clubs)
+    
+    else:
+        id = request.form['id']
+        password = request.form['pass']
+        name = request.form['name']
+        belonging = request.form['belonging']
+        area = request.form['area']
+        category = request.form['category']
+        club = Club(id='{}'.format(id), password='{}'.format(password), name='{}'.format(name), belonging='{}'.format(belonging), area='{}'.format(area), category='{}'.format(category))
+        session.add(club)
+        session.commit()
+        return redirect("/club_register")
+
+
+
+@app.route("/clubpage", methods=["GET", "POST"])
+def clubpage():
+    return render_template("clubpage.html", club=access_club)
 
 
 
